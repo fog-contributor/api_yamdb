dcf4bfea8f7cf9aa9d9edb5d8682d3bfd48d250d
Стас Балджи <stanislavbaldzhy@yandex.ru>
2022-10-18 15:39:31 +0400

    Добавил модели:
    -Category;
    -Genre;
    -Title.
    Зарегистрировал их в админке.

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import User
+from .models import Category, Genre, Title, User
 
 admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Title)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,12 +1,20 @@
 from django.contrib.auth.models import AbstractUser
+# from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
-ROLE = (('user', 'Пользователь'), ('moderator', 'Модератор'), ('admin', 'Администратор'))
+ROLE = (
+    ('user', 'Пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Администратор')
+)
+SCORE_CHOICES = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 
 class User(AbstractUser):
-    # password = None
+    """
+    Кастомная модель user c уникальным username и email.
+    """
     email = models.EmailField(
         max_length=254,
         unique=True,
@@ -27,4 +35,87 @@ class User(AbstractUser):
             'Администратор, модератор или пользователь. По умолчанию user.'
         ),
         blank=True
+    )
+
+
+class Category(models.Model):
+    """
+    Категории (типы) произведений.
+    """
+    name = models.CharField('Название', max_length=256)
+    slug = models.SlugField('Slug', max_length=50)
+
+    def __str__(self):
+        return self.name
+
+# class Comment(models.Model):
+#     """"""
+#     title = models.ForeignKey(
+#         'Title',
+#         on_delete=models.CASCADE,
+#         related_name='comments',
+#         verbose_name='ID произведения'
+#     )
+#     author = models.ForeignKey(
+#         'User',
+#         on_delete=models.CASCADE,
+#         related_name='comments',
+#         verbose_name='Автор'
+#     )
+#     text = models.TextField('Текст комментария')
+#     pub_date = models.DateTimeField(
+#         'Дата публикации',
+#         auto_now_add=True,
+#     )
+
+
+class Genre(models.Model):
+    """
+    Добавление жанра.
+    """
+    name = models.CharField('Название жанра', max_length=128)
+    slug = models.SlugField('Slug', max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+# class Review(models.Model):
+#     """
+#     Добавить новый отзыв. Пользователь может оставить только один отзыв.
+#     """
+#     title_id = models.ForeignKey(
+#         # 'ID произведения',
+#         'Title',
+#         on_delete=models.CASCADE,
+#         related_name='reviews'
+#     )
+#     text = models.CharField('Текст отзыва', max_length=256)
+#     score = models.IntegerField(
+#         'Оценка от 1 до 10',
+#         validators=[MinValueValidator(1), MaxValueValidator(10)],
+#         # choices=SCORE_CHOICES
+#     )
+
+
+class Title(models.Model):
+    """
+    Произведения, к которым пишут отзывы (определённый фильм, книга или песенка).
+    """
+    name = models.CharField('Название', max_length=128)
+    year = models.DateTimeField('Год выпуска')
+    genre = models.ForeignKey(
+        'Genre',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='titles',
+        verbose_name='Slug жанра'
+
+    )
+    description = models.TextField('Описание', blank=True)
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='titles',
+        verbose_name='Slug категории'
     )
