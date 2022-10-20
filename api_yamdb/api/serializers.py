9b4db2844ba686295bba6be3150620ba3fc4dc9e
Игорь Жук <igor.zhuk.code@gmail.com>
2022-10-20 18:06:34 +0400

    Modify: - unique validation for ReviewSerializer. - unique constraint for Review model. - add dummy models User, Category & Title.

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,14 +1,33 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-from reviews.models import Comment, Review
+from rest_framework.validators import ValidationError
+from reviews.models import Comment, Review, Title
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(source='author.username')
+    title = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Review
         fields = ('id', 'title', 'text', 'author', 'score' 'pub_date',)
 
+    def validate(self, data):
+        if self.context['request'].method == 'PATCH':
+            return data
+        title = get_object_or_404(
+            Title, pk=self.context['view'].kwargs['title_id']
+        )
+        review_qs = title.review.filter(author=self.context['request'].user)
+        if review_qs.exists():
+            raise ValidationError('Можно оставить только один отзыв.')
+        return data
+
 
 class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(source='author.username')
+    review_id = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Comment
         fields = ('id', 'review', 'text', 'author', 'pub_date',)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,21 +1,45 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-SCORE_CHOICES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+class User(AbstractUser):
+    username = models.CharField()
+
+
+class Category(models.Model):
+    name = models.CharField()
+    slug = models.SlugField(unique=True,)
+
+
+class Title(models.Model):
+    name = models.CharField()
+    year = models.IntegerField()
+    category = models.ForeignKey(Category,)
 
 
 class Review(models.Model):
+    SCORE_CHOICES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
     title = models.ForeignKey(
-        Title, on_delete=models.CASCADE, related_name='review'
+        Title, on_delete=models.CASCADE, related_name='reviews'
     )
-    text = models.TextField
+    text = models.TextField()
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='review'
+        User, on_delete=models.CASCADE, related_name='reviews'
     )
     score = models.IntegerChoices(choices=SCORE_CHOICES)
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.text
+
+    class Meta:
+        ordering = ("-pub_date",)
+        constraints = [
+            models.UniqueConstraint(
+                fields=["title", "author"], name="unique_review"
+            ),
+        ]
 
 
 class Comment(models.Model):
@@ -30,3 +54,6 @@ class Comment(models.Model):
 
     def __str__(self) -> str:
         return self.text
+
+    class Meta:
+        ordering = ("-pub_date",)
