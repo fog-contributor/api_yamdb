4cbd72f258bc7267514c205007cfb0b67cddfd2d
Krylov Arkadii <bukoka@yandex.ru>
2022-10-22 10:27:40 +0300

    fix dependences

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     'django_filters',
     'api.apps.ApiConfig',
     'reviews.apps.ReviewsConfig',
-    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -71,7 +71,7 @@ class Genre(models.Model):
 
 
 class Review(models.Model):
-    SCORE_CHOICES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    SCORE_CHOICES = ((1,1), (2,2), (3,3), (4,4), (5,5), (6,6), (7,7), (8,8), (9,9), (10,10))
 
     title = models.ForeignKey(
         'Title', on_delete=models.CASCADE, related_name='reviews'
@@ -80,7 +80,7 @@ class Review(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='reviews'
     )
-    score = models.IntegerChoices(choices=SCORE_CHOICES)
+    score = models.IntegerField(choices=SCORE_CHOICES)
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
