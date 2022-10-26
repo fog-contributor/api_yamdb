e4ba227e51ff42a1bb392c0fba5bfceed5ac1c28
fog-contributor <46422143+fog-contributor@users.noreply.github.com>
2022-10-26 13:07:20 +0300

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -5,7 +5,6 @@ from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework.views import APIView
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.permissions import (IsAuthenticated,
