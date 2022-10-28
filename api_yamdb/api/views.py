b0187cd7301b435d16659655ab95d4aed33e09c0
Krylov Arkadii <bukoka@yandex.ru>
2022-10-28 09:02:33 +0300

    serializer review

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -47,14 +47,14 @@ class UserViewSet(viewsets.ModelViewSet):
             permission_classes=(IsAuthenticated,))
     def me(self, request):
         me = request.user
-        serializer = UserSerializer(me)
+        serializer = self.get_serializer(me)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @me.mapping.patch
     def patch_me(self, request):
         me = request.user
-        serializer = UserSerializer(me, data=request.data, partial=True)
+        serializer = self.get_serializer(me, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save(role=me.role)
         return Response(serializer.data, status=status.HTTP_200_OK)
